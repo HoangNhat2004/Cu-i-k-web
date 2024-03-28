8b27e59e88c9a598c6eafd62c67a43879884fb98
HoangNhat2004 <164615124+HoangNhat2004@users.noreply.github.com>
2024-03-28 22:06:07 +0700

    Update header.php

--- header.php
+++ header.php
@@ -1,15 +1,11 @@
 <header class="header">
     <div class="header_body">
-        <a href="Final_Project.php" class="logo">TechnoBling</a>
+        <a href="index.php" class="logo">TechnoBling</a>
         <nav class="navbar">
-            <a href="Final_Project.php">Add Products</a>
+            <a href="index.php">Add Products</a>
             <a href="view_products.php">View Products</a>
             <a href="shop_products.php">Shopit</a>
         </nav>
-        <?php
-        $select_product = mysqli_query($conn,"Select * from `cart`") or die('query failed');
-        $row_count = mysqli_num_rows($select_product);
-        ?>
-        <a href="cart.php" class="cart"><i class="fa-solid fa-cart-shopping"></i><span><sup><?php echo $row_count?></sup></span></a>
+        <a href="" class="cart"><i class="fa-solid fa-cart-shopping"></i><span><sup></sup>4</span></a>
     </div>
 </header>
