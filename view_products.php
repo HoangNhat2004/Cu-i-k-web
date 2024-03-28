0361d55470267d725593ef89836b472db6c861ef
HoangNhat2004 <164615124+HoangNhat2004@users.noreply.github.com>
2024-03-28 22:04:22 +0700

    Update view_products.php

--- view_products.php
+++ view_products.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <?php include 'header.php'?>
+<?php include 'header.php'?>
     <div class="container">
         <section class="display_product">
                     <?php
@@ -30,7 +30,6 @@
                             <tr>
                                 <td><?php echo $num?></td>
                                 <td><img src="images/<?php echo $row['image']?>" alt=<?php echo $row['name']?>></td>
-                                <td><?php echo $row['name']?></td>
                                 <td><?php echo $row['name']?></td>
                                 <td><?php echo $row['price']?></td>
                                 <td>
